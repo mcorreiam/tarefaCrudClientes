8b754f3a447ce237630c17da3e3f8f27327f337b
Marco Correia <m.correia.m@gmail.com>
2021-05-31 17:36:46 -0300

    findAll paged

--- src/main/java/com/marcocorreia/trabalho/servicies/ClientService.java
+++ src/main/java/com/marcocorreia/trabalho/servicies/ClientService.java
@@ -9,6 +9,8 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -77,5 +79,12 @@ public class ClientService {
 			throw new DatabaseException("Integrity violation"); 
 		}
 		
+	}
+
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		
+		Page<Client> list = repository.findAll(pageRequest);
+		
+		return list.map(x -> new ClientDTO(x));
 	}
 }
